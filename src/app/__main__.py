12438b2e535d65fd9dccf758eabfe836ad310b2f
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2024-10-07 19:47:10 -0300

    haar funcionando

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,16 +1,38 @@
-"""
-This is the main entry point of the application.
+import cv2
+import matplotlib.pyplot as plt
 
-Import the bare minimum needed and add any initialization code here.
-"""
+#Plotar imagem
+def MostrarImagemComDeteccoes(imagem, objetos_detectados, cor=(0, 255, 0), nome="Imagem"):
+    for (x, y, w, h) in objetos_detectados:
+        cv2.rectangle(imagem, (x, y), (x + w, y + h), cor, 2)
+    plt.figure(figsize=(10, 6))
+    plt.imshow(cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB))
+    plt.title(nome)
+    plt.axis("off")
+    plt.show()
 
+#Aplicar Haar Cascade
+def AplicarClassificador(caminho_classificador, imagem, escala=1.1, vizinhos=5, min_tamanho=(30, 30)):
+    classificador = cv2.CascadeClassifier(cv2.data.haarcascades + caminho_classificador)
+    cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    objetos_detectados = classificador.detectMultiScale(cinza, scaleFactor=escala, minNeighbors=vizinhos, minSize=min_tamanho)
+    return objetos_detectados
 
 def main():
-    """
-    Main entry point of the application. This is where the application starts.
-    """
-    print("Hello, World!")
-    return 0
+
+    #parametros
+    parametros = {
+        'Rosto': [(1.1, 5)],
+    }
+
+    imagem_rosto = cv2.imread('src/app/imagem_teste.jpeg')
+    if imagem_rosto is None:
+        print("Erro ao carregar a imagem de rosto. Verifique o caminho.")
+    else:
+        for escala, vizinhos in parametros['Rosto']:
+            objetos_rosto = AplicarClassificador('haarcascade_frontalface_default.xml', imagem_rosto, escala=escala, vizinhos=vizinhos)
+            MostrarImagemComDeteccoes(imagem_rosto.copy(), objetos_rosto, nome=f"Rosto Detectado (escala={escala}, vizinhos={vizinhos})")
+        return 0
 
 
 if __name__ == "__main__":
